999f991f0f9e7c61f44df5e5215286798ddaa49b
Zilahi Alexandra <zilahi.alexandra04@gmail.com>
2025-09-22 21:27:39 +0300

    update app.py to use dedicated database user

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:54321@localhost:5432/kresz_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://kresz:kresz@localhost:5432/kresz_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
