e65561421b9a24bcc639f3ba3c2ee6d952d13adb
Zilahi Alexandra <zilahi.alexandra04@gmail.com>
2025-09-24 13:53:55 +0300

    remove routes.py, use only app.py, and add initial HTML page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -9,4 +9,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Ide importáld a models-t, miután a 'db' objektum létrejött.
 import models
+from models import Question, Answer
+
+# A route-ok maradjanak itt
+@app.route('/')
+def home():
+    questions = Question.query.all()
+    return render_template('home.html', questions=questions)
--- routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-from app import app
-from models import Question, Answer
-
-@app.route('/')
-def home():
-    questions = Question.query.all()
-    return render_template('home.html', questions=questions)
