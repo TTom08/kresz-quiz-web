b21f6cee7e863ed39ecaa89db88fc60a55db05a9
Tóth Tamás <t.tamas3208@gmail.com>
2025-09-26 12:33:54 +0200

    add submit endpoint to routes

--- routes.py
+++ routes.py
@@ -21,3 +21,28 @@ def start_quiz():
     db.session.commit()
 
     return jsonify({"message": f"Kvíz elindítva {username} számára"}), 201 # Created
+
+@quiz_bp.route("/submit", methods=["POST"])
+def submit_quiz():
+    data = request.get_json()
+    username = data.get("username")
+    score_value = data.get("score")
+
+    if not username or score_value is None:
+        return jsonify({"error": "Hiányzó adatok"}), 400
+
+    # Check user
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({"error": "Ismeretlen felhasználó"}), 404
+
+    # Saving score
+    new_score = Score(score=score_value, user_id=user.id)
+    db.session.add(new_score)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Pontszám elmentve",
+        "username": username,
+        "score": score_value
+    }), 201
