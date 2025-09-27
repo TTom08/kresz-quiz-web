04512340cf70a89104c0dc4da4a146753cfe9ddd
Zilahi Alexandra <zilahi.alexandra04@gmail.com>
2025-09-28 01:30:21 +0300

    file cleanup

--- kresz_quiz/test/conftest.py
+++ kresz_quiz/test/conftest.py
@@ -1,27 +1,22 @@
 import uuid
 
 import pytest
-# KIZÁRÓLAG a create_app függvényt importáljuk, nem a globális app példányt!
 from app import create_app, db
 from models import User, Question, Answer, Score
 
 @pytest.fixture(scope='session')
 def client():
-    # Itt hozza létre az alkalmazást a tesztkonfigurációval,
-    # amely felülírja az éles PostgreSQL URI-t.
     app = create_app({
         'TESTING': True,
-        'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:', # Elszigetelt, ideiglenes DB
+        'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:',
     })
 
     with app.app_context():
-        # A memóriában lévő DB tábláinak létrehozása
         db.create_all()
         yield app.test_client()
 
 @pytest.fixture(scope='function', autouse=True)
 def session_scope(client):
-    # Ez továbbra is gondoskodik a tesztek közötti rollbackről (takarításról)
     with client.application.app_context():
         yield db.session
         db.session.rollback()
